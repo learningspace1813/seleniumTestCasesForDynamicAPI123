6c841f2758a97a346d7ad9619fccf33ab0cf7da1
learningspace1813 <learningspace.go@gmail.com>
2025-08-20 15:35:50 +0530

    Update variables.java

--- src/test/java/profTravel/variables.java
+++ src/test/java/profTravel/variables.java
@@ -14,7 +14,7 @@ public class variables {
 	public static final String departureDate = "//input[@id='departure']";
 	public static final String departPlaceDD = "//input[@name='to']";
 	public static final String departPlace = "(//div[@class='most--popular-from fadein results-container-to']//div[2])";
-	public static final String travelCountDD = "//a[@data-toggle='dropdown']";
+	public static final String travelCountDD = "(//div[contains(@class,'dropdown dropdown-contain')])[1]";
 	public static final String travelCountIncre = "(//*[local-name()='svg' and contains(@stroke-linejoin,'round')])[14]";
 	public static final String searchFlight = "//button[@id='flights-search']";
 	
